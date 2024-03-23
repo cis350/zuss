885a7c0bdfb1e67c1af103a4cd9852308f28b240
emilyugc <emilyxcollab@gmail.com>
2024-03-23 16:33:19 -0400

    plz work

--- src/index.js
+++ src/index.js
@@ -9,18 +9,6 @@ app.get('/', (_req, res) => {
   res.send('hello');
 });
 
-app.get('/insert-sample-data', async (_req, res) => {
-  try {
-    await database.insertSampleData(); 
-    const data = await database.fetchClubsData(); // Fetch the sample data
-    res.json(data);
-  } catch (error) {
-    console.error('Error inserting sample data:', error);
-    res.status(500).send('Failed to insert sample data.');
-  }
-});
-
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
-
