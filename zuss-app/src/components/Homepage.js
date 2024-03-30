5b3a75ab5979981d0e55bb90f16579931ff10ab6
emilyugc <emilyxcollab@gmail.com>
2024-03-30 19:52:14 -0400

    add logout

--- zuss-app/src/components/Homepage.js
+++ zuss-app/src/components/Homepage.js
@@ -1,14 +1,21 @@
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Homepage() {
-    return (
-      <div>
-        <h1>Success!</h1>
-        <p>You have successfully logged in.</p>
-       
-      </div>
-    );
-  }
-  
-  export default Homepage;
-  
+  const navigate = useNavigate(); 
+
+  const handleLogout = () => { 
+    
+    navigate('/'); 
+  };
+
+  return (
+    <div>
+      <h1>Success!</h1>
+      <p>You have successfully logged in.</p>
+      <button onClick={handleLogout}>Logout</button> 
+    </div>
+  );
+}
+
+export default Homepage;
