51a5f33fdce05598bfe57213effc229d4cfae010
Clara <nolan.clara.us@gmail.com>
2024-04-14 16:06:43 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@
 // import the express app
 const app = require('./server');
 
-const port = 8080;
+const port = process.env.port || 5000;
 // start the web server
 app.listen(port, () =>{
     console.log('Server running on port', port);
