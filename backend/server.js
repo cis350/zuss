814cf4567f1f055a3e712d5ea8df51426ed054ea
Clara <nolan.clara.us@gmail.com>
2024-04-14 15:41:42 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,24 @@
-const express = require('express');
-const database = require('./database');
-
-// Import the Express webapp configuration
 const app = require('./index');
-
+const database = require('./database');
 const port = process.env.PORT || 8000;
 
-// Listen on the configured port
 const server = app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
 
+
 process.on('SIGINT', async () => {
     console.log('Closing database connection...');
-    await database.close();
+    await database.client.close();
     server.close(() => {
       console.log('HTTP server closed');
   });
-});
+  });
   
-process.on('SIGTERM', async () => {
+  process.on('SIGTERM', async () => {
     console.log('SIGTERM signal received: closing MongoDB connection');
-    await database.close();
+    await database.client.close(); // Assuming 'client' is exported from your database module
     server.close(() => {
         console.log('HTTP server closed');
     });
-});
-
-module.exports = app;
-
+  });
